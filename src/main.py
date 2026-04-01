12d46b5b1eead4730233be7f2b8d7408f2f7ae7c
Rúben Lisboa <rubenrlisboa@gmail.com>
2026-04-01 20:02:57 +0100

    Funcionalidade de visualizar o total de despesas e melhoria do menu

--- src/main.py
+++ src/main.py
@@ -26,19 +26,37 @@ def ver_despesas():
         print(f"Despesa: {row[0]}€ | Categoria:{row[1]} | Descrição:{row[2]} | Data:{row[3]}")
     
     file.close()
+
+def total_despesas():
+    total = 0;
+    file = open(FILE, "r")
+    leitor = csv.reader(file)
+    for row in leitor:
+        total+= float(row[0])
+    file.close()
+
+    print(f"\n Total de gastos: {total:.2f}€ \n")
 def main():
-    print("****** Gestor de finanças *******")
-    print("1-) Adicionar despesa")
-    print("2-) Ver despesas")
+    while True:
+        print("****** Gestor de finanças *******")
+        print("1-) Adicionar despesa")
+        print("2-) Ver despesas")
+        print("3-) Ver total de despesas")
+        print("0-) Sair do programa")
 
-    escolha = input("Escolha: ")
+        escolha = input("Escolha: ")
 
-    if escolha == "1":
-       add_despesa()
-    elif escolha== "2":
-        ver_despesas()
-    else:
-        print("Opção inválida")
+        if escolha == "1":
+            add_despesa()
+        elif escolha== "2":
+            ver_despesas()
+        elif escolha== "3":
+            total_despesas()
+        elif escolha == "0":
+            print("\n Muito obrigado por utlizador o programa FinanceGestor\n")
+            break
+        else:
+            print("Opção inválida")
 
 if __name__ == "__main__":
     main()
